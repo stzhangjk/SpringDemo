2ed9598c844429b92be6a97c1e011ca2f6a7f3d9
Grady0000 <stzhangjk@163.com>
2016-08-06 15:57:51 +0800

    @Transaction属性

--- 015-TransactionManage/src/main/java/com/stzhangjk/springdemo/service/UserService.java
+++ 015-TransactionManage/src/main/java/com/stzhangjk/springdemo/service/UserService.java
@@ -5,6 +5,7 @@ import com.stzhangjk.springdemo.dao.UserDAO;
 import com.stzhangjk.springdemo.entity.Log;
 import com.stzhangjk.springdemo.entity.User;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -18,13 +19,23 @@ public class UserService {
     private UserDAO dao;
     private LogDAO logDAO;
 
-    @Transactional
+    //@Transactional(propagation = Propagation.MANDATORY)//调用之前必须有事务，否则报异常
+    @Transactional(propagation = Propagation.REQUIRED)//默认值，如果已经有事务，则添加，没有再新创建事务
+    //@Transactional(propagation = Propagation.NESTED)//内嵌的事务
+    //@Transactional(propagation = Propagation.NEVER)//不能有事务，否则报异常
+    //@Transactional(propagation = Propagation.NOT_SUPPORTED)//不能有外层事务，如果有则挂起外层，执行完再让外层继续
     public void add(User user){
         dao.insert(user);
         Log log = new Log();
         log.setMessage(user.getUsername() + " inserted!");
         logDAO.insert(log);
         System.out.println("add "+user.getUsername()+" done!");
+    }
+
+    //rollbackfor ,什么情况下回滚
+    @Transactional(readOnly = true)//只读，建立只读Connection，效率比较高
+    public User getUser(int id){
+        return null;
     }
 
     public UserDAO getDao() {
